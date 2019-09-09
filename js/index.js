dcb7e1732219e8ec1aa356bbedc38fa3b1b77bb6
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-09-08 22:30:12 -0600

    this is what i have so far

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const newNavs = document.querySelectorAll(".nav-link");
+//mouse over
+newNavs.forEach((nav) => {
+  nav.addEventListener("mouseover", function(event) {
+    nav.style.color = "red";
+    nav.style.transition = "transform 0.5s";
+  }); //mouseout
+  nav.addEventListener("mouseout", (event) => {
+    nav.style.color = "black";
+    nav.style.transition = "transform 0.5s";
+  }); //click
+  nav.addEventListener("click", (e) => {
+    nav.textContent = "Radical";
+    console.log(changeNav);
+  });
+});
+
+
+
+ const images = document.querySelectorAll('img');
+//wheel
+images.forEach((picture) => {
+    images.addEventListener('zoom', (event) => {
+     images.style.transform = 'scale(${scale})';   
+    });
+});
