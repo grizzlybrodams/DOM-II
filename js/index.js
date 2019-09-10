2cd0400c7f4ff1f81e7206c08a20e51bf39f962a
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-09-09 21:30:24 -0600

    added more events

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const newNavs = document.querySelectorAll(".nav-link");
 //mouse over
 newNavs.forEach((nav) => {
   nav.addEventListener("mouseover", function(event) {
-    nav.style.color = "red";
+    nav.style.color = "purple";
     nav.style.transition = "transform 0.5s";
   }); //mouseout
   nav.addEventListener("mouseout", (event) => {
@@ -17,11 +17,41 @@ newNavs.forEach((nav) => {
 });
 
 
+const image = document.querySelectorAll('img');
 
- const images = document.querySelectorAll('img');
-//wheel
-images.forEach((picture) => {
-    images.addEventListener('zoom', (event) => {
-     images.style.transform = 'scale(${scale})';   
-    });
+image.forEach((pic) => {
+  image[0].addEventListener('drag', (event) => {
+    image[0].src = "img/cowabunga.jpg";
+  });
+  image[0].addEventListener('drop', (event) => {
+    image[0].src = "img/fun-bus.jpg";
+  });
+  image[1].addEventListener('click', (event) => {
+    image[1].src = "img/ninja turtle.jpg";
+  });
+  image[2].addEventListener('dblclick', (event) => {
+    image[2].src = "img/pizza-time.png";
+  });
+});
+
+const title = document.querySelectorAll('h2');
+
+title.forEach((words) =>{
+  title[0].addEventListener('mouseover', (event) =>{
+    title[0].textContent = "Cowabunga!!";
+  });
+  title[1].addEventListener('mouseover', (event) =>{
+    title[1].textContent = "ninja turtles!";
+  });
+  title[2].addEventListener('mouseover', (event) =>{
+    title[2].textContent = "Pizza time!";
+  });
+});
+
+
+
+const backGround = document.querySelector('body');
+
+backGround.addEventListener('resize', (event) => {
+  backGround.style.color = 'black';
 });
